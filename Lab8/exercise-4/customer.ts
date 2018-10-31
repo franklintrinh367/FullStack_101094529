560253f92ed045406d36f5d3febbeb0a457e3143
Franklin Trinh <franklintrinh367@gmail.com>
2018-10-31 17:32:44 -0400

    Add files via upload

---
+++ Lab8/exercise-4/customer.ts
@@ -0,0 +1,18 @@
+class Customer {
+    private firstname: string;
+    private lastname: string;
+
+    constructor(firstname, lastname){
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+
+    public greeter(){
+        console.log(`Hello ${this.firstname} ${this.lastname}`);
+    }
+}
+
+let customer = new Customer("Quan", "Trinh");
+customer.greeter();
+
+export class Customer;
