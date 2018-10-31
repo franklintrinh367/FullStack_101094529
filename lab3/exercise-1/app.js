3a32608449f88606aac4a9f4a36912756289a51b
Franklin Trinh <franklintrinh367@gmail.com>
2018-10-31 17:03:08 -0400

    Create app.js

---
+++ lab3/exercise-1/app.js
@@ -0,0 +1,14 @@
+// require built-in file system module
+var fs = require('fs');
+
+// create readable stream to read in the file
+var stream = fs.createReadStream("data.txt");
+
+// subscribe to 'data' emitter in readable stream 
+stream.on("data", function(data) {
+    console.log(data);
+})
+
+
+
+*** this is some sample lab data in text file ****
